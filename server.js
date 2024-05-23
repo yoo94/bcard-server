6ba0ad5c67f6c6d0ad7357dbd08a644bfeb52059
yoo94 <jaeseok9405@gmail.com>
2024-05-23 10:04:11 +0900

    -최적화

--- server.js
+++ server.js
@@ -13,54 +13,58 @@ const mongoUrl = process.env.MONGO_URL; // .env 파일에 MONGO_URL을 설정하
 app.use(cors());
 app.use(express.json());
 
+let client;
+async function connectToDatabase() {
+    if (!client) {
+        client = new MongoClient(mongoUrl, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            serverSelectionTimeoutMS: 5000,
+            maxPoolSize: 10 // 연결 풀 크기 설정
+        });
+        await client.connect();
+    }
+    return client.db('bcard').collection('bcardlist');
+}
+
 // 기본 라우트
 app.get('/', (req, res) => {
     res.send('서버가 정상적으로 작동 중입니다.');
 });
 
 app.post('/onCreate', async (req, res) => {
-    const { _id, name, hpNum, company, email, image, auth } = req.body;
-    const client = new MongoClient(mongoUrl);
+    const { name, hpNum, company, email, image, auth } = req.body;
 
     try {
-        await client.connect();
-        const database = client.db('bcard');
-        const collection = database.collection('bcardlist');
-        const data = {
-            name: name,
-            hpNum: hpNum,
-            company: company,
-            email: email,
-            image: image,
-            auth: auth
-        }
-        const result = await collection.insertOne(data);
+        const collection = await connectToDatabase();
+
+        const result = await collection.insertOne({
+            name,
+            hpNum,
+            company,
+            email,
+            image,
+            auth
+        });
 
         res.status(200).send(result);
     } catch (error) {
         console.error('Error inserting data:', error);
         res.status(500).send({ error: '데이터 삽입 중 오류가 발생했습니다.' });
-    } finally {
-        await client.close();
     }
 });
 
 app.post('/selectData', async (req, res) => {
     const { auth } = req.body;
-    const client = new MongoClient(mongoUrl);
-    const data = { auth: auth }
-    try {
-        await client.connect();
-        const database = client.db('bcard');
-        const collection = database.collection('bcardlist');
 
-        const users = await collection.find(data).toArray();
+    try {
+        const collection = await connectToDatabase();
+
+        const users = await collection.find({ auth }).toArray();
         res.status(200).send(users);
     } catch (error) {
-        console.error('Error retrieving data:', error); // 추가: 오류 로그
+        console.error('Error retrieving data:', error);
         res.status(500).send({ error: '데이터 출력 중 오류가 발생했습니다.' });
-    } finally {
-        await client.close();
     }
 });
 
